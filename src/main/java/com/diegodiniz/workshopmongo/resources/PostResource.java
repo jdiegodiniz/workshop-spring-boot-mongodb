837cc8394bda63ea0c72acf57a9220b3c3edffeb
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-26 14:19:09 -0300

    Consulta simple com query methods

--- src/main/java/com/diegodiniz/workshopmongo/resources/PostResource.java
+++ src/main/java/com/diegodiniz/workshopmongo/resources/PostResource.java
@@ -2,10 +2,13 @@ package com.diegodiniz.workshopmongo.resources;
 
 import com.diegodiniz.workshopmongo.domain.Post;
 
+import com.diegodiniz.workshopmongo.resources.util.URL;
 import com.diegodiniz.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -18,6 +21,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
